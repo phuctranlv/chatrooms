92a7a8afcb62139c0a087ca1fadf4965811ef388
Phuc Tran <phuctranlv@gmail.com>
2020-05-31 18:22:35 -0700

    Add initial react components and css module

---
+++ client/src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from '../styles/App.css';
+
+
+const App = () => {
+  return(
+    <div>
+      <div className={styles.test}>
+        Hello World
+      </div>
+    </div>
+  )
+}
+
+export default App;
