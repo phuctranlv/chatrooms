9c5b9356a43eead2249501440f273fa1f4555c70
Phuc Tran <phuctranlv@gmail.com>
2020-06-02 13:38:17 -0700

    Clean up code

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import styles from '../styles/App.css';
 
 const socket = io();
-
 const { username, room } = Qs.parse(location.search, { ignoreQueryPrefix: true });
 
 class App extends React.Component {
@@ -17,15 +16,15 @@ class App extends React.Component {
       speakButton: '',
       recognition: ''
     }
-    this.onSubmitHandler =  this.onSubmitHandler.bind(this);
+    this.onSubmitHandler = this.onSubmitHandler.bind(this);
     this.onChangeHandler = this.onChangeHandler.bind(this);
-    this.onSubmitLocationHandler = this.onSubmitLocationHandler.bind(this);
     this.autoScroll = this.autoScroll.bind(this);
-    
+    this.onSpeakHandler = this.onSpeakHandler.bind(this);
+
     socket.emit('join', { username, room }, (error) => {
       if (error) {
         alert(error);
-        location.href = '/'
+        location.href = '/';
       };
     });
 
@@ -40,22 +39,8 @@ class App extends React.Component {
       this.autoScroll();
     });
 
-    socket.on('locationMessage', (message) => {
-      this.setState({
-        messages: [...this.state.messages, {
-          username: message.username,
-          createdAt: moment(message.createdAt).format('h:mm a'),
-          url: message.url
-        }]
-      });
-      this.autoScroll();
-    });
-
     socket.on('roomData', ({ room, users }) => {
-      this.setState({
-        room,
-        users
-      })
+      this.setState({ room, users })
     })
   }
 
@@ -66,39 +51,38 @@ class App extends React.Component {
 
     if (typeof SpeechRecognition !== "undefined") {
       this.state.recognition = new SpeechRecognition();
+
       const stop = () => {
         this.state.recognition.stop();
         this.state.speakButton.textContent = "Start speech";
         this.state.speakButton.classList.remove("speaking")
-        this.setState({
-          listening: false
-        })
+        this.setState({ listening: false })
       };
+
       const start = () => {
         this.state.recognition.start();
-        this.state.speakButton.textContent = "Stop speed";
+        this.state.speakButton.textContent = "Stop speech";
         this.state.speakButton.classList.add("speaking")
-        this.setState({
-          listening: true
-        })
+        this.setState({ listening: true })
       };
+
       const onResult = (event) => {
         let speechToText = ''
+
         for (const res of event.results) {
           speechToText = speechToText + res[0].transcript + ' ';
+
           if (res.isFinal) {
             socket.emit('sendMessage', speechToText, (message) => {
-              if (message) {
-                console.log(message);
-              } else {
-                console.log('The message was delivered successfully!');
-              }
+              message ? console.log(message) : console.log('The message was delivered successfully!');
             })
             speechToText = '';
           }
         }
+
         stop();
       };
+
       this.state.recognition.continuous = false;
       this.state.recognition.interimResults = false;
       this.state.recognition.addEventListener("result", onResult);
@@ -109,9 +93,7 @@ class App extends React.Component {
   }
 
   onChangeHandler(event) {
-    this.setState({
-      message: event.target.value
-    })
+    this.setState({ message: event.target.value })
   }
 
   onSubmitHandler(event) {
@@ -125,76 +107,36 @@ class App extends React.Component {
     socket.emit('sendMessage', this.state.message, (message) => {
       submitButton.removeAttribute('disabled');
       formInput.focus();
-
-      if (message) {
-        console.log(message);
-      } else {
-        console.log('The message was delivered successfully!');
-      }
+      message ? console.log(message) : console.log('The message was delivered successfully!');
     })
 
-    this.setState({
-      message: ''
-    })
+    this.setState({ message: '' })
   }
 
   onSpeakHandler(event) {
     event.preventDefault();
-
-  }
-
-  onSubmitLocationHandler(event) {
-    event.preventDefault();
-
-    const submitButton = event.target[0];
-    
-    if (!navigator.geolocation) {
-      return alert('Geolocation is not supported by your browser.');
-    }
-
-    submitButton.setAttribute('disabled', 'disabled');
-
-    navigator.geolocation.getCurrentPosition((position) => {
-      const location = {
-        long: position.coords.longitude,
-        lat: position.coords.latitude
-      }
-
-      socket.emit('sendLocation', location, () => {
-        submitButton.removeAttribute('disabled');
-
-        console.log('Your location was shared successfully!')
-      })
-    })
   }
 
   autoScroll() {
     const $messages = document.getElementsByClassName('chat__messages');
-
     const $newMessage = $messages[0].lastElementChild;
-
     // height of new message:
     const newMessageStyle = getComputedStyle($newMessage);
     const newMessageMargin = parseInt(newMessageStyle.marginBottom);
     const newMessageHeight = $newMessage.offsetHeight + newMessageMargin;
-
     // visible height:
     const visibleHeight = $messages[0].offsetHeight;
-
     // height of messages container:
     const containerHeight = $messages[0].scrollHeight;
-
     // how far have I scrolled:
     const scrollOffset = $messages[0].scrollTop + visibleHeight;
-
     if (containerHeight - newMessageHeight <= scrollOffset) {
       $messages[0].scrollTop = $messages[0].scrollHeight;
     }
   }
 
   render() {
-
-    return(
+    return (
       <div className="chat">
         <div className="chat__sidebar">
           <h2 className="room-title">
@@ -203,52 +145,21 @@ class App extends React.Component {
           </h2>
           <h3 className="list-title">Users:</h3>
           <ul className="users">
-            {
-              this.state.users.map((user) => {
-                return (
-                  <li>{user.username}</li>
-                )
-              })
-            }
+            {this.state.users.map((user) => <li>{user.username}</li>)}
           </ul>
         </div>
         <div className="chat__main">
           <div className="chat__messages">
             {
               this.state.messages.map(message => {
-                let messageToDisplay;
-
-                if (message.url) {
-                  messageToDisplay = (
-                    <div>
-                      <p>
-                        <span className="message__name">{message.username}</span>
-                        <span className="message__meta">{message.createdAt}</span>
-                      </p>
-                      <p>
-                        <a 
-                          href={`${message.url}`} 
-                          target="_blank"
-                        >
-                          Location shared
-                        </a>
-                      </p>
-                    </div>
-                  )
-                } else {
-                  messageToDisplay = (
-                    <div>
-                      <p>
-                        <span className="message__name">{message.username}</span>
-                        <span className="message__meta">{message.createdAt}</span>
-                      </p>
-                      <p>{message.message}</p>
-                    </div>
-                  )
-                }
-                
                 return (
-                  <div className="message">{messageToDisplay}</div>
+                  <div className="message">
+                    <p>
+                      <span className="message__name">{message.username}</span>
+                      <span className="message__meta">{message.createdAt}</span>
+                    </p>
+                    <p>{message.message}</p>
+                  </div>
                 )
               })
             }
@@ -257,15 +168,15 @@ class App extends React.Component {
             <form onSubmit={this.onSubmitHandler}>
               <input
                 type="text"
-                placeholder="Type text here" 
-                value={this.state.message} 
-                onChange={this.onChangeHandler} 
+                placeholder="Type text here"
+                value={this.state.message}
+                onChange={this.onChangeHandler}
                 required
                 autoComplete="off"
               />
               <button>Submit text</button>
             </form>
-            <form onSubmit={(event) => event.preventDefault()}>
+            <form onSubmit={this.onSpeakHandler}>
               <button id="speakButton">Start speech</button>
             </form>
           </div>
