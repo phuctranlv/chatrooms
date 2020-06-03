617a5862e7f1d151dc5a3be83e77dd3c5d1a5eb0
Phuc Tran <phuctranlv@gmail.com>
2020-06-03 02:11:21 -0700

    Remove location message generator function

--- server/utilities/messages.js
+++ server/utilities/messages.js
@@ -1,20 +1,9 @@
-const generateMessage = (username, text) => {
-  return {
-    username,
-    text,
-    createdAt: new Date().getTime()
-  }
-}
-
-const generateLocationMessage = (username, lat, long) => {
-  return {
-    username,
-    url: `https://google.com/maps?q=${lat},${long}`,
-    createdAt: new Date().getTime()
-  }
-}
+const generateMessage = (username, text) => ({
+  username,
+  text,
+  createdAt: new Date().getTime()
+});
 
 module.exports = {
-  generateMessage,
-  generateLocationMessage
-}
+  generateMessage
+};
