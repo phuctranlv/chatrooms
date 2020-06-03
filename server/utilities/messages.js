37f2c60d6d7f9be866ac157fd8abe5f866de4c09
Phuc Tran <phuctranlv@gmail.com>
2020-06-03 14:26:21 -0700

    Add chats in-memory-storage capability

--- server/utilities/messages.js
+++ server/utilities/messages.js
@@ -1,10 +1,33 @@
-const generateMessage = (username, user, text) => ({
-  username,
-  text,
-  createdAt: new Date().getTime(),
-  color: user.color
-});
+const moment = require('moment');
+
+const chats = {};
+
+const generateMessage = (username, user, text) => {
+  const message = {
+    username,
+    message: text,
+    createdAt: moment(new Date().getTime()).format('h:mm a'),
+    color: user.color
+  };
+
+  const { room } = user;
+  if (!chats[room]) {
+    chats[room] = [];
+  } else if (username !== 'Admin') {
+    chats[room].push(message);
+  }
+
+  return message;
+};
+
+const getMessages = (room) => {
+  if (chats[room]) {
+    return chats[room];
+  }
+  return [];
+};
 
 module.exports = {
-  generateMessage
+  generateMessage,
+  getMessages
 };
