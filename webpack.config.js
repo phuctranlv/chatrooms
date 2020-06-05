ac8954264dc3f1c9b6247b52e86f55c1a99daeb0
Phuc Tran <phuctranlv@gmail.com>
2020-06-04 21:30:17 -0700

    Modify entry point name extension to jsx

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './client/src/index.js',
+  entry: './client/src/index.jsx',
   output: {
     path: path.resolve(__dirname, 'client/public'),
     filename: 'bundle.js'
