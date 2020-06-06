837374f187fd831cb74aea12d46d401f80124a18
Phuc Tran <phuctranlv@gmail.com>
2020-06-05 19:37:20 -0700

    Create mutateMessage and deleteMessage functions

--- server/utilities/messages.js
+++ server/utilities/messages.js
@@ -1,3 +1,5 @@
+const fullOperationTransform = require('./algorithm.js');
+
 const chats = {};
 
 const generateMessage = (username, userId, user, text) => {
@@ -21,6 +23,50 @@ const generateMessage = (username, userId, user, text) => {
   return message;
 };
 
+const mutateMessage = (mutateObject, room) => {
+  let result;
+  if (!chats[room][mutateObject.conversationId]) {
+    chats[room].push(mutateObject);
+    result = chats[room][mutateObject.conversationId].text;
+  } else {
+    const mutationArray = chats[room][mutateObject.conversationId].mutations;
+    const transformedObject = fullOperationTransform(mutateObject, mutationArray);
+    if (transformedObject.data.type === 'insert' && transformedObject.data.index < 0) {
+      transformedObject.data.index = 0;
+      const message = chats[room][mutateObject.conversationId];
+      message.text = transformedObject.data.text;
+    } else if (transformedObject.data.type === 'delete' && transformedObject.data.index < 0) {
+      transformedObject.data.index = 0;
+      transformedObject.data.length = 0;
+    } else if (transformedObject.data.type === 'insert') {
+      const message = chats[room][mutateObject.conversationId];
+      message.text = message.text.split('').splice(transformedObject.data.index, 0, transformedObject.data.text).join('');
+    } else {
+      const message = chats[room][mutateObject.conversationId];
+      message.text = message.text.split('').splice(transformedObject.data.index, transformedObject.data.length).join('');
+      if (message.text.length === 0) {
+        transformedObject.data.index = 0;
+        transformedObject.data.length = 0;
+      }
+    }
+    mutationArray.push(transformedObject);
+    result = chats[room][mutateObject.conversationId].text;
+  }
+  return result;
+};
+
+const deleteMessage = (conversationId, room) => {
+  const conversations = chats[room];
+  const index = conversations.find((conversation) => conversation.id === conversationId);
+  if (index === -1) {
+    return undefined;
+  }
+  const deletedChat = chats[room].splice(index, 1);
+  return deletedChat;
+
+
+};
+
 const getMessages = (room) => {
   if (chats[room]) {
     return chats[room];
@@ -37,5 +83,7 @@ const getMessages = (room) => {
 
 module.exports = {
   generateMessage,
+  mutateMessage,
+  deleteMessage,
   getMessages
 };
