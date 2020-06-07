dbb3a856f6cdfa3ef6607ae64af9b4aecbd171fc
Phuc Tran <phuctranlv@gmail.com>
2020-06-06 23:36:10 -0700

    Change functions from promises to callback pattern

--- database/index.js
+++ database/index.js
@@ -14,119 +14,132 @@ client.connect((error) => {
 
 const cassandraDb = {};
 
-cassandraDb.generateConversation = (id, username, text, createdat, color, lastmutationid) => {
-  const queryParam = [id, username, text, createdat, color, lastmutationid];
-  const query = 'insert into conversations (id, username, text, createdat, color, lastmutationid) values (?, ?, ? ,?, ?, ?)';
-
-  return new Promise((resolve, reject) => {
-    client.execute(query, queryParam, { prepare: true }, (err, result) => {
-      if (err) {
-        console.log('error from generating conversation:', err);
-        return reject(err);
-      }
-      console.log('result from generating conversation:', result);
-      resolve(result);
-    });
+cassandraDb.insertConversation = (id, username, text, createdat, color, lastmutationid, lastmutationobject, cb) => {
+  const queryParam = [id, username, text, createdat, color, lastmutationid, lastmutationobject];
+  const query = 'insert into conversations (id, username, text, createdat, color, lastmutationid, lastmutationobject) values (?, ?, ? ,?, ?, ?, ?)';
+  client.execute(query, queryParam, { prepare: true }, (err, result) => {
+    if (err) {
+      // console.log('error from generating conversation:', err);
+      cb(err, null);
+    } else {
+      // console.log('result from generating conversation:', result);
+      cb(null, result);
+    }
   });
 };
 
-cassandraDb.checkConversationExistence = (id) => {
+cassandraDb.checkConversationExistence = (id, cb) => {
   const queryParam = [id];
   const query = 'select * from conversations where id = ?;';
 
-  return new Promise((resolve, reject) => {
-    client.execute(query, queryParam, { prepare: true }, (err, result) => {
-      if (err) {
-        console.log('error from checking conversation existence:', err);
-        return reject(err);
-      }
+  client.execute(query, queryParam, { prepare: true }, (err, result) => {
+    if (err) {
+      console.log('error from checking conversation existence:', err);
+      cb(err, null);
+    } else {
       console.log('result from checking conversation existence:', result);
-      resolve(result);
-    });
+      cb(null, result);
+    }
   });
 };
 
-cassandraDb.insertConversationMutation = (id, username, createdat, color, mutationid, mutationindex, length, text, type, originalice, originbob) => {
+cassandraDb.insertConversationMutation = (id, username, createdat, color, mutationid, mutationindex, length, text, type, originalice, originbob, cb) => {
   const queryParam = [id, username, createdat, color, mutationid, mutationindex, length, text, type, originalice, originbob];
   const query = 'insert into mutations (id, username, createdat, color, mutationid, mutationindex, length, text, type, originalice, originbob) values (?, ?, ? ,?, ?, ?, ?, ?, ? ,?, ?)';
 
-  return new Promise((resolve, reject) => {
-    client.execute(query, queryParam, { prepare: true }, (err, result) => {
-      if (err) {
-        console.log('error from mutating conversation:', err);
-        return reject(err);
-      }
+  client.execute(query, queryParam, { prepare: true }, (err, result) => {
+    if (err) {
+      console.log('error from mutating conversation:', err);
+      cb(err, null);
+    } else {
       console.log('result from mutating conversation:', result);
-      resolve(result);
-    });
+      cb(null, result);
+    }
   });
 };
 
-cassandraDb.getAllMutationsOfAConversation = (id) => {
+cassandraDb.getAllMutationsOfAConversation = (id, cb) => {
   const queryParam = [id];
   const query = 'select * from mutations where id = ?;';
-
-  return new Promise((resolve, reject) => {
-    client.execute(query, queryParam, { prepare: true }, (err, result) => {
-      if (err) {
-        console.log('error from getting all mutations of a conversation:', err);
-        return reject(err);
-      }
+  
+  client.execute(query, queryParam, { prepare: true }, (err, result) => {
+    if (err) {
+      console.log('error from getting all mutations of a conversation:', err);
+      cb(err, null);
+    } else {
       console.log('result from getting all mutations of a conversation:', result);
-      resolve(result);
-    });
+      cb(null, result);
+    }
   });
 };
 
-cassandraDb.deleteConversation = (id) => {
+cassandraDb.deleteConversation = (id, cb) => {
   const queryParam = [id];
   const query = 'delete from conversations where id = ?;';
 
-  return new Promise((resolve, reject) => {
-    client.execute(query, queryParam, { prepare: true }, (err, result) => {
-      if (err) {
-        console.log('error from deleting conversation:', err);
-        return reject(err);
-      }
-      resolve(result);
+  client.execute(query, queryParam, { prepare: true }, (err, result) => {
+    if (err) {
+      console.log('error from deleting conversation:', err);
+      cb(err, null);
+    } else {
+      cb(null, result);
       console.log('result from deleting conversation:', result);
-    });
+    }
   });
 };
 
-cassandraDb.getAllConversations = () => {
+cassandraDb.getAllConversations = (cb) => {
   const query = 'select * from conversations;';
 
-  return new Promise((resolve, reject) => {
-    client.execute(query, (err, result) => {
-      if (err) {
-        console.log('error from getting all conversations:', err);
-        return reject(err);
-      }
-      console.log('result from getting all conversations:', result);
-      resolve(result);
-    });
+  client.execute(query, (err, result) => {
+    if (err) {
+      // console.log('error from getting all conversations:', err);
+      cb(err, null);
+    } else {
+      // console.log('result from getting all conversations:', result);
+      cb(null, result);
+    }
   });
 };
 
-const id = 'phuctranlv1234';
-const username = 'phuctranlv';
-const text = 'test test test';
-const createdat = '1234';
-const color = 'rbg(somenumber,somenumber,somenumber)';
-const lastmutationid = 3;
-const mutationid = 4;
-const mutationindex = 5;
-const length = '4';
-const type = 'delete';
-const originalice = 4;
-const originbob = 3;
+// const id = 'phuctranlv1234';
+// const username = 'phuctranlv';
+// const text = 'test test test';
+// const createdat = '1234';
+// const color = 'rbg(somenumber,somenumber,somenumber)';
+// const lastmutationid = 3;
+// const lastmutationobject = JSON.stringify({
+//   author: 'alice | bob',
+//   conversationId: 'string',
+//   data: {
+//     index: 3,
+//     length: 'number | undefined',
+//     text: 'string | undefined',
+//     type: 'insert | delete'
+//   },
+//   origin: {
+//     alice: 'integer',
+//     bob: 'integer'
+//   }
+// });
+// const mutationid = 4;
+// const mutationindex = 5;
+// const length = '4';
+// const type = 'delete';
+// const originalice = 4;
+// const originbob = 3;
 
-cassandraDb.generateConversation(id, username, text, createdat, color, lastmutationid);
-cassandraDb.getAllConversations();
-// cassandraDb.deleteConversation(id);
-cassandraDb.checkConversationExistence('3');
-cassandraDb.insertConversationMutation(id, username, createdat, color, mutationid, mutationindex, length, text, type, originalice, originbob);
-cassandraDb.getAllMutationsOfAConversation(id);
+// const callback = (error, result) => {
+//   if (error) console.log('Theres an error. the error is:', error);
+//   if (result) console.log('success! the result is:', result);
+// };
 
-module.exports = { cassandraDb };
+// cassandraDb.insertConversation(id, username, text, createdat, color, lastmutationid, lastmutationobject, callback);
+// cassandraDb.getAllConversations(callback);
+
+// // cassandraDb.deleteConversation(id);
+// cassandraDb.checkConversationExistence(id, callback);
+// cassandraDb.insertConversationMutation(id, username, createdat, color, mutationid, mutationindex, length, text, type, originalice, originbob, callback);
+// cassandraDb.getAllMutationsOfAConversation(id, callback);
+
+module.exports = cassandraDb;
